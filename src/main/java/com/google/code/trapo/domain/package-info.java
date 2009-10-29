3077b75548b8ec7e7cbf894508b094df0dbc0eed
Bamboozle Who <bamboozle.who@gmail.com>
2009-10-28 23:58:28 -0200

    removing unecessary typedefs and adding named queries

--- src/main/java/com/google/code/trapo/domain/package-info.java
+++ src/main/java/com/google/code/trapo/domain/package-info.java
@@ -1,31 +1,18 @@
 @TypeDefs({
 	@TypeDef(name = "datetime", typeClass = PersistentDateTime.class),
-	@TypeDef(name = "datetimetz", typeClass = PersistentDateTimeTZ.class),
-	@TypeDef(name = "duration", typeClass = PersistentDuration.class),
-	@TypeDef(name = "interval", typeClass = PersistentInterval.class),	
-	@TypeDef(name = "localdate", typeClass = PersistentLocalDate.class),	
-	@TypeDef(name = "localdatetime", typeClass = PersistentLocalDateTime.class),	
-	@TypeDef(name = "localtimeasstring", typeClass = PersistentLocalTimeAsString.class),	
-	@TypeDef(name = "localtimeastime", typeClass = PersistentLocalTimeAsTime.class),	
-	@TypeDef(name = "localtimeexact", typeClass = PersistentLocalTimeExact.class),	
-	@TypeDef(name = "period", typeClass = PersistentPeriod.class),	
-	@TypeDef(name = "timeofday", typeClass = PersistentTimeOfDay.class),	
-	@TypeDef(name = "timeofdayexact", typeClass = PersistentTimeOfDayExact.class)	
+})
+@NamedQueries({
+	@NamedQuery(
+		name = "Forum.byName", 
+		query = "from Forum f where f.name = ?"
+	)
 })
 package com.google.code.trapo.domain;
+
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 
 import org.hibernate.annotations.TypeDef;
 import org.hibernate.annotations.TypeDefs;
 import org.joda.time.contrib.hibernate.PersistentDateTime;
-import org.joda.time.contrib.hibernate.PersistentDateTimeTZ;
-import org.joda.time.contrib.hibernate.PersistentDuration;
-import org.joda.time.contrib.hibernate.PersistentInterval;
-import org.joda.time.contrib.hibernate.PersistentLocalDate;
-import org.joda.time.contrib.hibernate.PersistentLocalDateTime;
-import org.joda.time.contrib.hibernate.PersistentLocalTimeAsString;
-import org.joda.time.contrib.hibernate.PersistentLocalTimeAsTime;
-import org.joda.time.contrib.hibernate.PersistentLocalTimeExact;
-import org.joda.time.contrib.hibernate.PersistentPeriod;
-import org.joda.time.contrib.hibernate.PersistentTimeOfDay;
-import org.joda.time.contrib.hibernate.PersistentTimeOfDayExact;
 
